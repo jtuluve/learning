3713e43d12ce6ceaadfbf2bdce48051a51bfcb7e
jtuluve <74163310+jtuluve@users.noreply.github.com>
2022-05-14 11:55:15 +0530

    Update cvpython.py

--- trial/cvpython.py
+++ trial/cvpython.py
@@ -1,17 +1,7 @@
+print "Content-type: text/html\n\n";
+
 import cgi
-import cgitb #found this but isn't used?
 
 form = cgi.FieldStorage()
 
-first_name = form.getvalue('first_name').capitalize()
-last_name  = form.getvalue('last_name').capitalize()
-
-print ("Content-type:text/html\r\n\r\n")
-print ("<html>")
-print ("<head>")
-print ("<title>Hello - Second CGI Program</title>")
-print ("</head>")
-print ("<body>")
-print ("<h2>Your name is {}. {} {}</h2>".format(last_name, first_name, last_name))
-print ("</body>")
-print ("</html>")
+username = form.getvalue("username")
